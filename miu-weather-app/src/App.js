62105dad340f42c3661dcae81e8d5216e0abf04f
Phat Nguyen <phatnguyentanit@gmail.com>
2022-09-28 13:59:07 -0500

    Updated Weather App

--- miu-weather-app/src/App.js
+++ miu-weather-app/src/App.js
@@ -1,21 +1,14 @@
 import './App.css';
 import Dashboard from "./components/Dashboard";
-import AppRouter from "./AppRouter";
 import {BrowserRouter} from "react-router-dom";
-import { ThemeColorContext } from "./ThemeColorContext";
-import {useState} from "react";
+
 
 function App() {
-    const [themeColorState, setThemeColorState] = useState('Navy');
 
       return (
         <div className="App">
-            <div>Current Theme Color: {themeColorState}</div>
             <BrowserRouter>
-                <ThemeColorContext.Provider value={{themeColorState, setThemeColorState}}>
-                    <Dashboard />
-                    <AppRouter />
-                </ThemeColorContext.Provider>
+                <Dashboard />
             </BrowserRouter>
         </div>
       );
--- miu-weather-app/src/components/Dashboard.js
+++ miu-weather-app/src/components/Dashboard.js
@@ -1,14 +1,23 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import {useState} from "react";
+import { ThemeColorContext } from "../ThemeColorContext";
+import AppRouter from "../AppRouter";
 
 export default function Dashboard() {
+
+    const [themeColorState, setThemeColorState] = useState('Navy');
 
     return (
 
         <div>
-            <ul>
-                <li><Link to='/weathers'>Weathers</Link></li>
-                <li><Link to='/add-job'>Add Job</Link></li>
-            </ul>
+            <div>Current Theme Color: {themeColorState}</div>
+            <ThemeColorContext.Provider value={{themeColorState, setThemeColorState}}>
+                <ul>
+                    <li><Link to='/weathers'>Weathers</Link></li>
+                    <li><Link to='/add-job'>Add Job</Link></li>
+                </ul>
+                <AppRouter />
+            </ThemeColorContext.Provider>
         </div>
     )
 }
